467b506b01f82322b3bdc9d7a8f2a728ec480e29
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-08-25 14:03:41 +0300

    commit tree 9aeddeaa5c6f686a8ab652ef83bc536f97fd8204

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
 
+  attr_accessor :total, :discount, :items, :last_transaction
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def add_item(title, amount, quantity = 1)
+    self.last_transaction = amount * quantity
+    self.total += self.last_transaction
+    quantity.times { self.items << title }
+  end
+#keep in mind the to_f, and to_i method
+  def apply_discount
+    if self.discount != 0
+      self.total = (self.total * (100.0 - self.discount.to_f) / 100).to_i
+      "After the discount, the total comes to $#{self.total}."
+    else
+      "There is no discount to apply."
+    end
+  end
+
+  def void_last_transaction
+    self.total -= self.last_transaction
+  end
+end
